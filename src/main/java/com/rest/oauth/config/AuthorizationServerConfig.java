05722b50e5e46f22b7b5ff339e8943b492d94f4b
calillo <calillo@email.com>
2018-07-17 18:09:46 +0200

    spring oauth2 server in memory

---
+++ src/main/java/com/rest/oauth/config/AuthorizationServerConfig.java
@@ -0,0 +1,53 @@
+package com.rest.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Autowired
+    private AuthenticationManager authenticationManager;
+	 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+        		.withClient("client_id")
+        		.secret(passwordEncoder().encode("client_secret"))
+        		//.secret("client_secret")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
+                //.authorities("ROLE_READ", "ROLE_WRITE")
+                .scopes("read", "write")
+                .accessTokenValiditySeconds(5000)
+                .refreshTokenValiditySeconds(50000);
+    }
+    
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+        		 .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){ 
+        return new BCryptPasswordEncoder(); 
+    }
+
+}
---
+++ src/main/java/com/rest/oauth/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.rest.oauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+ 
+	@Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+	
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder().encode("user"))
+            .roles("USER")
+            .authorities("CAR_READ")
+        .and()
+        	.withUser("admin")
+        	.password(passwordEncoder().encode("admin"))
+        	.roles("ADMIN")  
+        	.authorities("CAR_READ", "CAR_WRITE");
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){ 
+        return new BCryptPasswordEncoder(); 
+    }
+    
+}
